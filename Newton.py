8f9bbc2a40454128a1592e996364a08d7a9606d8
Martin Tanseco <martin_tanseco@berkeley.edu>
2024-08-22 11:36:46 -0700

    doesnt work

--- Newton.py
+++ Newton.py
@@ -1,18 +1,18 @@
 import math
-def func(k):
-    return math.sin(k*2)
+def funct(k):
+    return math.sin(k*2) # Edit this function
 
-def d1(k):
+def d1(k,func):
     return func(k+0.001)/0.001 - func(k)/0.001
 
 def d2(k):
     return d1(k+0.001)/0.001 - d1(k)/0.001
 
-def optimize(k):
+def optimize(k,func):
     k1 = 0
     k0 = 0
     while not abs(k1-k0) > 0.00001:
         k0=k
-        k1 = k -d1(k)/d2(2)
+        k1 = k -d1(k,func)/d2(k)
         k = k1
     return [k, func(k)]
