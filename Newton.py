55d81fae20a248a7171ab9169c48d795da5e50d0
Martin Tanseco <martin_tanseco@berkeley.edu>
2024-08-22 13:25:46 -0700

    Added Documation

--- Newton.py
+++ Newton.py
@@ -5,19 +5,23 @@ def funct(k):
     return math.sin(k * 2)  # Edit this function
 
 
-def d1(k, func):
-    return func(k + 0.001) / 0.001 - func(k) / 0.001
+def d1(k, func, h = 1e-6): # Derivative function
+    return func(k + h) / h - func(k) / h
 
 
-def d2(k, func):
-    return d1(k + 0.001, func) / 0.001 - d1(k, func) / 0.001
+def d2(k, func, h = 1e-6): # 2nd Derivative function
+    return d1(k + h, func) / h - d1(k, func) / h
 
 
-def optimize(k, func):
+def optimize(k, func, h = 1e-6): # Newton Method
     k1 = 1
     k0 = 0
-    while abs(k1 - k0) > 0.00001:
+    count = 0
+    while abs(k1 - k0) > 0.001: # Approximation algorithm
         k0 = k
         k1 = k - d1(k, func) / d2(k, func)
         k = k1
+        count += 1
+        if count > 1e9: # Setting limit on while loop
+            break
     return [k, func(k)]
